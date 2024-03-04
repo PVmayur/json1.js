fd3d90ceae76cca0ece783350ab9d13402820394
Mayur <113410133+PVmayur@users.noreply.github.com>
2024-03-04 08:24:10 +0000

    update

---
+++ basic1/01_variabals.js
@@ -0,0 +1,21 @@
+const studentId= 22232
+
+console.log(studentId);
+
+let nameId = "Mayur"
+var last = "patil"
+let suId=null
+
+//prefer not to use var//
+//becuse of isssue in block ans=d functional scope//
+//documentaion two website//
+/*
+1)tc39.es
+2)mdn web docs
+*/
+city = "surat"
+let mayur;
+console.table([nameId,last,city,mayur]);
+
+console.table([typeof nameId,last,suId,mayur])
+console.log(typeof nameId,last,suId,mayur)
